7baa61741569ba4de7fbe6d72173c8bbed1d0a11
Yael Terrettaz <yterrettaz@wrk198.wrk.lsn.camptocamp.com>
2024-11-22 20:28:23 +0100

    [IMP] Manage different format type

--- sync.py
+++ sync.py
@@ -36,14 +36,12 @@ def read_yaml_file(file_path):
 
 def get_xmlid(client, model, record):
     """Get the XML ID for a record, if it exists, with special handling for product.template."""
-    # Vérifier si le modèle est product.template
     if model == 'product.template':
-        # Recherche d'une variante associée
         ProductVariant = client.env['product.product']
         variant_ids = ProductVariant.search([('product_tmpl_id', '=', record.id)])
         
         if variant_ids:
-            variant = ProductVariant.browse(variant_ids[0])  # Prendre la première variante (si plusieurs existent)
+            variant = ProductVariant.browse(variant_ids[0])
             xmlid = variant.get_external_id()
             for xmlid_key in xmlid.values():
                 if xmlid_key:
@@ -52,18 +50,14 @@ def get_xmlid(client, model, record):
             print(f"No variants found for product.template ID {record.id}.")
             return None
     
-    # Comportement standard pour les autres modèles
     xmlid = record.get_external_id()
     for xmlid_key in xmlid.values():
         if xmlid_key:
             return xmlid_key
-        else:
-            print('Not found XML ID')
     return None
 
-
 def create_xmlid(client, model, record, complete_xmlid_name=None):
-    """Create a new XML ID for a record in the specified model using the provided complete XML ID name."""
+    """Create a new XML ID for a record in the specified model."""
     if not complete_xmlid_name:
         module = '__migration__'
         complete_xmlid_name = f"{module}.{model.replace('.', '_')}_{record.id}"
@@ -81,7 +75,6 @@ def create_xmlid(client, model, record, complete_xmlid_name=None):
         'model': model,
         'res_id': record.id,
     })
-    print('New XML ID created:', complete_xmlid_name)
     return complete_xmlid_name
 
 def create_or_update_record(client, model, xmlid, values):
@@ -101,12 +94,23 @@ def get_field_type(model, field_name):
     model_fields = model.fields_get([field_name])
     return model_fields[field_name]['type']
 
-def process_char_field(record, field_name):
+def get_target_field_type(client, model, field_name):
+    """Retrieve the type of a field in a given model for the target instance."""
+    model_fields = client.env[model].fields_get([field_name])
+    return model_fields[field_name]['type']
+
+def process_char_field(record, field_name, field_type=None):
     """Process a char, text, or selection field."""
     field_value = record[field_name]
     if isinstance(field_value, str):
-        field_value = field_value.replace('\r\n', '<br/>').replace('\n', '<br/>').replace('\r', '<br/>')
+        if field_type == 'html':
+            field_value = field_value.replace('\r\n', '<br/>').replace('\n', '<br/>').replace('\r', '<br/>')
     return field_value
+
+def process_date_field(record, field_name):
+    """Process a date field."""
+    date_value = record[field_name]
+    return date_value if date_value else None  # Return None if the date is not set
 
 def process_many2one_field(odoo_source, odoo_target, record, field_name):
     """Process a many2one field and return the ID of the related record in the target instance."""
@@ -114,7 +118,7 @@ def process_many2one_field(odoo_source, odoo_target, record, field_name):
     if related_record:
         related_xmlid = get_xmlid(odoo_source, related_record._name, related_record)
         if not related_xmlid:
-            print(f"Error: Missing XML ID for related record {related_record.id} in model {related_record._name}. Skipping this field.")
+            print(f"Error: Missing XML ID for related record {related_record.id}. Skipping.")
             return None
         return odoo_target.env.ref(related_xmlid).id
     return None
@@ -126,36 +130,11 @@ def process_many2many_field(odoo_source, odoo_target, record, field_name):
     for related_record in related_records:
         related_xmlid = get_xmlid(odoo_source, related_record._name, related_record)
         if not related_xmlid:
-            print(f"Warning: Missing XML ID for related record {related_record.id} in model {related_record._name}. Skipping this related record.")
+            print(f"Warning: Missing XML ID for related record {related_record.id}. Skipping.")
             continue
         target_id = odoo_target.env.ref(related_xmlid).id
         related_ids.append(target_id)
     return [(6, 0, related_ids)]
-
-def process_boolean_field(record, field_name):
-    """Process a boolean field."""
-    return record[field_name]
-
-def process_date_field(record, field_name):
-    """Process a date field and return it in string format."""
-    date_value = record[field_name]
-    if date_value:
-        return date_value.strftime('%Y-%m-%d')  # Odoo expects date strings in this format
-    return None
-
-def process_datetime_field(record, field_name):
-    """Process a datetime field and return it in string format."""
-    datetime_value = record[field_name]
-    if datetime_value:
-        return datetime_value.strftime('%Y-%m-%d %H:%M:%S')  # Odoo expects datetime strings in this format
-    return None
-
-def process_float_field(record, field_name):
-    """Process a float field."""
-    float_value = record[field_name]
-    if float_value is not None:
-        return float(float_value)
-    return None
 
 def sync_model(datas=None):
     """Synchronize models defined in the configuration file."""
@@ -173,11 +152,6 @@ def sync_model(datas=None):
             print("Source or target model missing in configuration.")
             continue
 
-        user_input = input(f"Do you want to continue with the source node '{data_name}'? (y/n): ")
-        if user_input.lower() == 'n':
-            print("Operation canceled by the user.")
-            continue
-
         Records = odoo_source.env[source_model]
         filters = data.get('filter', [])
         limit = data.get('limit', None)
@@ -187,14 +161,12 @@ def sync_model(datas=None):
         record_ids = Records.search(filters, limit=limit, order='id asc')
 
         if not record_ids:
-            print(f"No records found for model '{source_model}' with the given filter and limit.")
+            print(f"No records found for model '{source_model}'.")
             continue
 
         total_records = len(record_ids)
         print(f"Total records found: {total_records}")
         
-        field_mappings = data.get('field_mappings', {})
-
         for batch_start in range(0, total_records, batch_size):
             batch_end = min(batch_start + batch_size, total_records)
             batch_ids = record_ids[batch_start:batch_end]
@@ -215,36 +187,23 @@ def sync_model(datas=None):
                     else:
                         field_source = field_target = field
 
-                    field_type = get_field_type(Records, field_source)
+                    source_field_type = get_field_type(Records, field_source)
+                    target_field_type = get_target_field_type(odoo_target, target_model, field_target)
 
-                    # Apply field mapping if defined
-                    if field_source in field_mappings:
-                        mapping = field_mappings.get(field_source)
-                        source_value = record[field_source]
-                        if source_value in mapping:
-                            values[field_target] = mapping[source_value]
-                        else:
-                            values[field_target] = record[field_source]
+                    if source_field_type == 'char' and target_field_type == 'html':
+                        values[field_target] = process_char_field(record, field_source, field_type='html')
+                    elif source_field_type in ['char', 'selection']:
+                        values[field_target] = process_char_field(record, field_source)
+                    elif source_field_type in ['integer', 'float']:
+                        values[field_target] = record[field_source]
+                    elif source_field_type == 'date':
+                        values[field_target] = process_date_field(record, field_source)
+                    elif source_field_type == 'many2one':
+                        values[field_target] = process_many2one_field(odoo_source, odoo_target, record, field_source)
+                    elif source_field_type == 'many2many':
+                        values[field_target] = process_many2many_field(odoo_source, odoo_target, record, field_source)
                     else:
-                        # Normal field processing
-                        if field_type in ['char', 'text', 'selection', 'integer']:
-                            values[field_target] = process_char_field(record, field_source)
-                        elif field_type == 'many2one':
-                            related_id = process_many2one_field(odoo_source, odoo_target, record, field_source)
-                            if related_id is not None:
-                                values[field_target] = related_id
-                        elif field_type == 'many2many':
-                            related_ids = process_many2many_field(odoo_source, odoo_target, record, field_source)
-                            if related_ids:
-                                values[field_target] = related_ids
-                        elif field_type == 'boolean':
-                            values[field_target] = process_boolean_field(record, field_source)
-                        elif field_type == 'date':
-                            values[field_target] = process_date_field(record, field_source)
-                        elif field_type == 'datetime':
-                            values[field_target] = process_datetime_field(record, field_source)
-                        elif field_type == 'float':
-                            values[field_target] = process_float_field(record, field_source)
+                        print(f"Unsupported field type: {source_field_type}")
 
                 create_or_update_record(odoo_target, target_model, source_xmlid, values)
 
